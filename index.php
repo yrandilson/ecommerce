c8fe960e6117e4189d8a4cbcbd777578fe9bd9c2
Irandilson2020 <you@example.com>
2020-03-30 17:36:46 -0300

    Passo 6 - Criamos o PageAdmin e Tpl Admin

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php 
 
 require_once("vendor/autoload.php");
+use \Slim\Slim;
+use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new \Slim\Slim();
 
@@ -9,6 +12,14 @@ $app->config('debug', true);
 $app->get('/', function() {
     
 	$page = new Hcode\Page();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
