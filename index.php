9d8287b042950da71e200d8bbfb8eaf2882eb954
Irandilson2020 <you@example.com>
2020-03-31 08:40:04 -0300

    Passo 7 - Tela de login

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
+require_once("functions.php");
+
+
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new \Slim\Slim();
 
@@ -19,9 +23,40 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
     
-	$page = new PageAdmin();
+	User::verifyLogin();
+
+	$page = new Hcode\PageAdmin();
 
 	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function() {
+    
+	$page = new Hcode\PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+
+	User::login(post('deslogin'), post('despassword'));
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 
 });
 
--- vendor/hcodebr/php-classes/src/DB/Sql.php
+++ vendor/hcodebr/php-classes/src/DB/Sql.php
@@ -6,7 +6,7 @@ class Sql {
 
 	const HOSTNAME = "127.0.0.1";
 	const USERNAME = "root";
-	const PASSWORD = " ";
+	const PASSWORD = "58iran";
 	const DBNAME = "db_ecommerce";
 
 	private $conn;
